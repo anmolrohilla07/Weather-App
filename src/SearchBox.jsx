a5d654faeba832ceb769a0677be4ae8e361ebd76
Anmol Rohilla <anmolrohilla07@gmail.com>
2026-06-20 01:15:20 +0530

    initialize weather API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,6 +4,23 @@ import { useState } from 'react';
 
 export default function SearchBox (){
     let [city, setCity] = useState("");
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_Key = "dcb9a1721e1e4871adcedc5649bf678d";
+ 
+    let getWeatherInfo = async(city) => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_Key}&units=metric`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let result = {
+            temp : jsonResponse.main.temp,
+            tempMin : jsonResponse.main.temp_min,
+            tempMax : jsonResponse.main.temp_max,
+            humidity : jsonResponse.main.humidity,
+            feelsLike : jsonResponse.main.feels_like,
+            weather : jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    }
 
     let handleChange = (event) => {
         setCity(event.target.value);
@@ -13,6 +30,7 @@ export default function SearchBox (){
         event.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo(city);
     }
 
     return (
