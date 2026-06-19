7012231d072274f48a00daccf60857651745d89a
Anmol Rohilla <anmolrohilla07@gmail.com>
2026-06-19 23:41:30 +0530

    Added Search Bar

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,11 +1,29 @@
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 export default function SearchBox (){
+    let [city, setCity] = useState("");
+
+    let handleChange = (event) => {
+        setCity(event.target.value);
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+    }
+
     return (
         <div>
             <h1>Search for a weather</h1>
-            <form action="">
-                <TextField id="city" label="City Name" variant="outlined" />
+            <form onSubmit={handleSubmit}>
+                <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange} required/>
+                <br /><br />
+                <Button variant="contained" type='submit'>
+                Search
+                </Button>
             </form>
         </div>
     )
